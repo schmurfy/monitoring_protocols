fee9e184450313ecfb132d92d508473380249d13
Julien Ammous <schmurfy@gmail.com>
2016-02-09 11:03:33 +0100

    do not allow more than 3 decimals

--- lib/monitoring_protocols/parser.rb
+++ lib/monitoring_protocols/parser.rb
@@ -12,7 +12,7 @@ module MonitoringProtocols
     def self.parse_and_validate_value(v)
       case v
       when Fixnum, Bignum     then v
-      when Float, BigDecimal  then v
+      when Float, BigDecimal  then ((v * 1000).to_i) / 1000.0
       else
         raise ParseError, "invalid value: #{v} [#{v.class}]"
       end
